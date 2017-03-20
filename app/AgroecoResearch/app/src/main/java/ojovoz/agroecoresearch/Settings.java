48999f74148e1b776f316824569fbc6d7fb17a4c
ojovoz <cubo23@yahoo.com>
2017-03-20 17:30:17 +0100

    Update
    Added 'Control panel' button on Settings screen

--- app/AgroecoResearch/app/src/main/java/ojovoz/agroecoresearch/Settings.java
+++ app/AgroecoResearch/app/src/main/java/ojovoz/agroecoresearch/Settings.java
@@ -2,14 +2,16 @@ package ojovoz.agroecoresearch;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Eugenio on 15/03/2017.
  */
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements httpConnection.AsyncResponse {
 
     public int userId;
     public int userRole;
@@ -53,6 +55,21 @@ public class Settings extends AppCompatActivity {
         dlg.show();
     }
 
+    public void controlPanel(View v){
+        httpConnection http = new httpConnection(this,this);
+        if (http.isOnline()) {
+            String url = server + "/control/";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            if(i.resolveActivity(getPackageManager())!=null) {
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        } else {
+            Toast.makeText(this, R.string.pleaseConnectMessage, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
     public void logout(View v){
         final Context context = this;
         Intent i = new Intent(context, LoginScreen.class);
@@ -67,5 +84,10 @@ public class Settings extends AppCompatActivity {
         i.putExtra("userRole",userRole);
         startActivity(i);
         finish();
+    }
+
+    @Override
+    public void processFinish(String output){
+
     }
 }
